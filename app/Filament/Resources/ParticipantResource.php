ed4f74ebd2acdbedb1cbec4997360294a02fbf57
Robbin Schepers <robbin@deschepers.nl>
2023-04-24 12:44:50 +0200

    chore: Reproducable bug

---
+++ app/Filament/Resources/ParticipantResource.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ParticipantResource\Pages;
+use App\Filament\Resources\ParticipantResource\RelationManagers;
+use App\Models\Participant;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ParticipantResource extends Resource
+{
+    protected static ?string $model = Participant::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Fieldset::make('User data')
+                    ->relationship('user')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->autofocus()
+                            ->required(),
+                        Forms\Components\TextInput::make('email')
+                            ->unique(ignoreRecord: true)
+                            ->required(),
+                        Forms\Components\TextInput::make('password')
+                            ->required()
+                            ->password(),
+                    ]),
+                Forms\Components\Toggle::make('active')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListParticipants::route('/'),
+            'create' => Pages\CreateParticipant::route('/create'),
+            'edit' => Pages\EditParticipant::route('/{record}/edit'),
+        ];
+    }
+}
